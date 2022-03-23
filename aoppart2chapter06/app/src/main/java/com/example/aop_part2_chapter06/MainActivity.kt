d89ac435e5fd8b741a9eb82fb689346c496a8a54
KIMJIMIN2 <jimin101477@naver.com>
2022-03-23 14:42:47 +0900

    2022-03-23

--- aoppart2chapter06/app/src/main/java/com/example/aop_part2_chapter06/MainActivity.kt
+++ aoppart2chapter06/app/src/main/java/com/example/aop_part2_chapter06/MainActivity.kt
@@ -1,46 +1,136 @@
 package com.example.aop_part2_chapter06
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private val remainMinutesTextView: TextView by lazy{
+    private val remainMinutesTextView: TextView by lazy {
         findViewById(R.id.remainMinutesTextView)
     }
-
-    private val remainSecondsTextView: TextView by lazy{
+    private val remainSecondsTextView: TextView by lazy {
         findViewById(R.id.remainSecondsTextView)
     }
-
-    private val seekBar: SeekBar by lazy{
+    private val seekBar: SeekBar by lazy {
         findViewById(R.id.seekBar)
     }
+    private var currentCountDownTimer: CountDownTimer? = null
+
+    private val soundPool = SoundPool.Builder().build()
+    private var tickingSoundId: Int? = null
+    private var bellSoundID: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+        initSounds()
     }
 
-    private fun bindViews(){
+    override fun onResume() {
+        super.onResume()
+        soundPool.autoResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.autoPause()
+    }
+
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundID = soundPool.load(this, R.raw.timer_bell, 1)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
+
+    private fun bindViews() {
         seekBar.setOnSeekBarChangeListener(
-            object:SeekBar.OnSeekBarChangeListener{
-                override fun onProgressChanged(seekBar:SeekBar?,progress:Int, fromUser:Boolean) {
-                    remainMinutesTextView.text = "$progress"
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        updateRemainTime(progress * 60 * 1000L)
+                    }
                 }
 
-                override fun onStartTrackingTouch(p0: SeekBar?) {
-                    TODO("Not yet implemented")
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    stopCountDown()
                 }
 
-                override fun onStopTrackingTouch(p0: SeekBar?) {
-                    TODO("Not yet implemented")
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar ?: return
+
+                    if (seekBar.progress == 0) {
+                        stopCountDown()
+                    } else {
+                        startCountDown()
+                    }
                 }
             }
         )
     }
+
+    private fun createCountDownTimer(initialMillis: Long) =
+        object : CountDownTimer(initialMillis, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                completeCountDown()
+            }
+
+        }
+
+    private fun startCountDown() {
+        currentCountDownTimer =
+            createCountDownTimer(seekBar.progress * 60 * 1000L).start()
+        currentCountDownTimer?.start()
+
+        tickingSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+        }
+    }
+
+    private fun stopCountDown() {
+        currentCountDownTimer?.cancel()
+        currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
+    private fun completeCountDown() {
+        updateRemainTime(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundID?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
+        }
+    }
+
+    private fun updateRemainTime(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+
+        remainMinutesTextView.text = "%02d'".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
+    }
+
+
 }
