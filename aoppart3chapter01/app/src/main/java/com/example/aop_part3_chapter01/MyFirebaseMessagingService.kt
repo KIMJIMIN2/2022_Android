9c632fd58c4b6b3f03a9d6bb8c2eb8736fd62541
KIMJIMIN2 <jimin101477@naver.com>
2022-03-28 23:40:01 +0900

    2022-03-28

---
+++ aoppart3chapter01/app/src/main/java/com/example/aop_part3_chapter01/MyFirebaseMessagingService.kt
@@ -0,0 +1,16 @@
+package com.example.aop_part3_chapter01
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+
+}
