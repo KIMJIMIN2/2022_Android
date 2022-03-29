b875cac11c6ea6c64063b2dbcdd8427865819fe5
KIMJIMIN2 <jimin101477@naver.com>
2022-03-29 16:26:34 +0900

    2022-03-29

--- aoppart3chapter01/app/src/main/java/com/example/aop_part3_chapter01/MainActivity.kt
+++ aoppart3chapter01/app/src/main/java/com/example/aop_part3_chapter01/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aop_part3_chapter01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -20,14 +21,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initFirebase()
+        updateResult()
     }
 
-    private fun initFirebase(){
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        updateResult(true)
+    }
+
+    private fun initFirebase() {
         FirebaseMessaging.getInstance().token
-            .addOnCompleteListener{ task->
-                if(task.isSuccessful){
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
                     firebase.text = task.result
                 }
+            }
+    }
+
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text =
+            (intent.getStringExtra("notificationType") ?: "앱 런처") + if (isNewIntent) {
+                "(으)로 갱신했습니다."
+            } else {
+                "(으)로 실행했습니다."
             }
     }
 }
